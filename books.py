1a7a929311a137e1c7cd0ee10c082d785ff5fd3a
erjav <erja.vaskivuori@helsinki.fi>
2022-12-03 17:35:02 +0200

    Added functions for borrowing books

--- books.py
+++ books.py
@@ -82,3 +82,25 @@ def get_reviews(book_id):
     sql = """SELECT username, score, comment FROM users INNER JOIN reviews
             ON users.id=user_id AND book_id=:book_id"""
     return db.session.execute(sql, {"book_id":book_id}).fetchall()
+
+def borrow_book(book_id, user_id, date):
+    sql = """INSERT INTO loans (book_id, user_id, date)
+            VALUES (:book_id, :user_id, :date)"""
+    db.session.execute(sql, {"book_id":book_id, "user_id":user_id, "date":date})
+    db.session.commit()
+    return True
+
+def return_book(book_id):
+    sql = "DELETE FROM loans WHERE book_id=:book_id"
+    db.session.execute(sql, {"book_id":book_id})
+    db.session.commit()
+    return True
+
+def get_loans_info(book_id):
+    sql = "SELECT * FROM loans WHERE book_id=:book_id"
+    return db.session.execute(sql, {"book_id":book_id}).fetchone()
+
+def get_all_loans():
+    sql = """SELECT username, name, author FROM users INNER JOIN loans ON users.id=user_id
+            INNER JOIN books ON books.id=book_id"""
+    return db.session.execute(sql).fetchall()
