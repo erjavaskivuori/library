2ffee8f207aa61e79ded66ec816de93d35da57c4
erjav <erja.vaskivuori@helsinki.fi>
2022-12-17 01:19:03 +0200

    Autoformatted

--- books.py
+++ books.py
@@ -1,104 +1,123 @@
 from db import db
 
+
 def get_all_books():
     sql = """SELECT id, name, author, year, genre FROM books
              WHERE visible='True'"""
     return db.session.execute(sql).fetchall()
 
+
 def get_book_details(book_id):
-    sql = """SELECT id, name, author, year, genre FROM books 
+    sql = """SELECT id, name, author, year, genre FROM books
              WHERE id=:id"""
-    return db.session.execute(sql, {"id":book_id}).fetchone()
+    return db.session.execute(sql, {"id": book_id}).fetchone()
+
 
 def order_books(order):
-    sql = """SELECT id, name, author, year, genre FROM books 
+    sql = """SELECT id, name, author, year, genre FROM books
              WHERE visible='True' ORDER BY %s"""
     return db.session.execute(sql, {order}).fetchall()
 
+
 def search_books_by_name(name):
-    sql = """SELECT id, name, author, year, genre FROM books 
-             WHERE visible = 'True' AND name LIKE :name"""    
-    return db.session.execute(sql, {"name":"%"+name+"%"}).fetchall()
+    sql = """SELECT id, name, author, year, genre FROM books
+             WHERE visible = 'True' AND name LIKE :name"""
+    return db.session.execute(sql, {"name": "%"+name+"%"}).fetchall()
+
 
 def search_books_by_author(author):
-    sql = """SELECT id, name, author, year, genre FROM books 
+    sql = """SELECT id, name, author, year, genre FROM books
              WHERE visible = 'True' AND author LIKE :author"""
-    return db.session.execute(sql, {"author":"%"+author+"%"}).fetchall()
+    return db.session.execute(sql, {"author": "%"+author+"%"}).fetchall()
+
 
 def search_books_by_year(year):
-    sql = """SELECT id, name, author, year, genre FROM books 
+    sql = """SELECT id, name, author, year, genre FROM books
              WHERE visible = 'True' AND year=:year"""
-    return db.session.execute(sql, {"year":year}).fetchall()
+    return db.session.execute(sql, {"year": year}).fetchall()
+
 
 def search_books_by_genre(genre):
-    sql = """SELECT id, name, author, year, genre FROM books 
+    sql = """SELECT id, name, author, year, genre FROM books
              WHERE visible = 'True' AND genre=:genre"""
-    return db.session.execute(sql, {"genre":genre}).fetchall()
+    return db.session.execute(sql, {"genre": genre}).fetchall()
+
 
 def add_book(name, author, year, genre):
 
     try:
-        sql = """INSERT INTO books (name, author, year, genre, visible) 
+        sql = """INSERT INTO books (name, author, year, genre, visible)
                 VALUES (:name, :author, :year, :genre, 'True')"""
-        db.session.execute(sql, {"name":name, "author":author,
-                "year":year, "genre":genre})
+        db.session.execute(sql, {"name": name, "author": author,
+                                 "year": year, "genre": genre})
         db.session.commit()
     except:
         return False
     return True
 
-def remove_book(id):
+
+def remove_book(book_id):
     sql = """UPDATE books SET visible='FALSE' WHERE id=:id"""
-    db.session.execute(sql, {"id":id})
+    db.session.execute(sql, {"id": book_id})
     db.session.commit()
 
     return True
+
 
 def wish_for_book(user_id, name, author):
     sql = """INSERT INTO wishlist (user_id, name, author)
             VALUES (:user_id, :name, :author)"""
-    db.session.execute(sql, {"user_id":user_id, "name":name, "author":author})
+    db.session.execute(
+        sql, {"user_id": user_id, "name": name, "author": author})
     db.session.commit()
 
     return True
+
 
 def get_wishes():
     sql = """SELECT username, name, author FROM users INNER JOIN wishlist
             ON users.id=user_id"""
     return db.session.execute(sql).fetchall()
 
+
 def add_review(book_id, user_id, score, comment):
     try:
-        sql = """INSERT INTO reviews (book_id, user_id, score, comment) 
+        sql = """INSERT INTO reviews (book_id, user_id, score, comment)
                  VALUES (:book_id, :user_id, :score, :comment)"""
-        db.session.execute(sql, {"book_id":book_id, "user_id":user_id,
-                         "score":score, "comment":comment})
+        db.session.execute(sql, {"book_id": book_id, "user_id": user_id,
+                                 "score": score, "comment": comment})
         db.session.commit()
         return True
     except:
         return False
 
+
 def get_reviews(book_id):
     sql = """SELECT username, score, comment FROM users INNER JOIN reviews
             ON users.id=user_id AND book_id=:book_id"""
-    return db.session.execute(sql, {"book_id":book_id}).fetchall()
+    return db.session.execute(sql, {"book_id": book_id}).fetchall()
+
 
 def borrow_book(book_id, user_id, date):
     sql = """INSERT INTO loans (book_id, user_id, date)
             VALUES (:book_id, :user_id, :date)"""
-    db.session.execute(sql, {"book_id":book_id, "user_id":user_id, "date":date})
+    db.session.execute(
+        sql, {"book_id": book_id, "user_id": user_id, "date": date})
     db.session.commit()
     return True
+
 
 def return_book(book_id):
     sql = "DELETE FROM loans WHERE book_id=:book_id"
-    db.session.execute(sql, {"book_id":book_id})
+    db.session.execute(sql, {"book_id": book_id})
     db.session.commit()
     return True
 
+
 def get_loans_info(book_id):
     sql = "SELECT * FROM loans WHERE book_id=:book_id"
-    return db.session.execute(sql, {"book_id":book_id}).fetchone()
+    return db.session.execute(sql, {"book_id": book_id}).fetchone()
+
 
 def get_all_loans():
     sql = """SELECT username, name, author FROM users INNER JOIN loans ON users.id=user_id
