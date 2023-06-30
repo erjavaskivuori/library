5ed05102f7bd8272f5ba1e8f0fe3628f17198837
erjav <erja.vaskivuori@helsinki.fi>
2023-06-30 14:26:52 +0300

    Updated to set environment variable

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 
 uri = getenv("DATABASE_URL")
+SECRET_KEY = getenv('SECRET_KEY', 'Optional default value')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
